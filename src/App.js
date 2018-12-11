fa44957823244e111e8948805f4d94498f187894
Sherry Li <shli@zendesk.com>
2018-12-12 08:47:43 +1100

    First commit for creating app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to our team</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <a className="logIn" href="#">Log In</a><b> | </b><a className="signUp" href="#">Sign Up</a>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
