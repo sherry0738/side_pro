2d3eeb85195fe7a1c565c3f58e9b644c644732f1
Sherry Li <shli@zendesk.com>
2018-12-12 15:21:25 +1100

    Create Quizzes component and get data from mockdb to display on the broswer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
+import Quizzes from './Quizzes';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <h1>Welcome to our team</h1>
-          <p>
-          <a className="logIn" href="#">Log In</a><b> | </b><a className="signUp" href="#">Sign Up</a>
-          </p>
-          
-        </header>
-      </div>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            quizzes: ''
+        };
+    }
 
+    componentDidMount() {
+        let mockUrl = 'https://wt-cbcccf288a9296cc9f9684fd6ba1f72f-0.sandbox.auth0-extend.com/empty-function';
+
+        fetch(mockUrl)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({ quizzes: res.quizzes });
+            });
+    }
+
+    render() {
+        let quizzes = 'loading';
+        if (this.state.quizzes) {
+            quizzes = this.state.quizzes.map((quiz, index) => {
+                return <Quizzes question_body={quiz.question_body} answers={quiz.answers} key={index} />;
+            });
+        }
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <div>
+                        <h1>Welcome to G-Aurora Team</h1>
+                    </div>
+                    <p><a href="#">Log in</a><strong> | </strong><a href="#">Sign up</a></p>
+                </div>
+                {quizzes}
+            </div>
+        );
+    }
+}
 export default App;
+
