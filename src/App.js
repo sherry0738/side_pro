286ff91b62ddc0ba99e9f2f5f371f2ce05a48b19
Sherry Li <shli@zendesk.com>
2018-12-12 17:22:44 +1100

    Install google login and Create googlelogin and googlelogout component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,110 @@
 import React, { Component } from 'react';
 import Quizzes from './Quizzes';
+import GoogleLogin from 'react-google-login';
+import GoogleLogout from 'react-google-login';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quizzes: ''
+            quizzes: '',
+            isLoggedIn: false,
+            avatarUrl: '',
         };
+        this.getTokenObj = this.getTokenObj.bind(this);
+        this.parseJwt = this.parseJwt.bind(this);
+    }
+
+    getTokenObj() {
+        return JSON.parse(localStorage.getItem('auth'));
+    }
+
+    parseJwt(token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace('-', '+').replace('_', '/');
+        return JSON.parse(window.atob(base64));
     }
 
     componentDidMount() {
-        let mockUrl = 'https://wt-cbcccf288a9296cc9f9684fd6ba1f72f-0.sandbox.auth0-extend.com/empty-function';
+        const tokenObj = this.getTokenObj();
+        console.log('tokenObj', tokenObj)
+        const idToken = tokenObj.id_token;
+        console.log('tokenObj.idToken', idToken)
+        tokenObj ? console.log('tokenObj', tokenObj) : console.log('no token');
+        if (tokenObj && idToken) {
+            this.setState({ isLoggedIn: true });
+            const decodedToken = this.parseJwt(idToken);
+            console.log('decodedToken', decodedToken);
 
-        fetch(mockUrl)
-            .then(res => res.json())
-            .then(res => {
-                this.setState({ quizzes: res.quizzes });
-            });
+            this.setState({ avatarUrl: decodedToken.picture });
+            //todo:move to service, only call after login
+            let mockUrl = 'http://localhost:8001';
+
+            fetch(mockUrl, { method: 'get', headers: new Headers({ Authorization: 'bearer ' + idToken }) })
+                .then(res => res.json())
+                .then(res => {
+                    this.setState({ quizzes: res.quizzes });
+                });
+        } else {
+            this.setState({ isLoggedIn: false });
+        }
     }
 
     render() {
         let quizzes = 'loading';
+
+        const onSuccess = response => {
+            this.setState({ isLoggedIn: true });
+            localStorage.setItem('auth', JSON.stringify(response.tokenObj));
+        };
+
+        const onFailure = () => {
+            this.setState({ isLoggedIn: false });
+            localStorage.removeItem('auth');
+        };
+
+        const logOut = () => {
+            this.setState({ isLoggedIn: false, avatarUrl: '' });
+            localStorage.removeItem('auth');
+        };
+
         if (this.state.quizzes) {
             quizzes = this.state.quizzes.map((quiz, index) => {
                 return <Quizzes question_body={quiz.question_body} answers={quiz.answers} key={index} />;
             });
         }
-
+        const isLoggedIn = this.state.isLoggedIn;
+        const avatarUrl = this.state.avatarUrl;
         return (
             <div className="App">
                 <div className="App-header">
+                    {isLoggedIn ? (
+                        <div>
+                            <img src={avatarUrl} />
+                            <GoogleLogout
+                                clientId="201605823214-a65bf5spbckkrhdvgsvu8get3p5jrhb5.apps.googleusercontent.com"
+                                buttonText="Logout"
+                                onSuccess={logOut}
+                            />
+                        </div>
+                    ) : (
+                            <GoogleLogin
+                                clientId="201605823214-a65bf5spbckkrhdvgsvu8get3p5jrhb5.apps.googleusercontent.com"
+                                buttonText="Login"
+                                onSuccess={onSuccess}
+                                onFailure={onFailure}
+                                isSignedIn={this.state.isLoggedIn}
+                            />
+                        )}
                     <div>
                         <h1>Welcome to G-Aurora Team</h1>
                     </div>
-                    <p><a href="#">Log in</a><strong> | </strong><a href="#">Sign up</a></p>
                 </div>
                 {quizzes}
             </div>
         );
     }
 }
-export default App;
 
+export default App;
